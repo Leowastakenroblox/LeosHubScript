21aa92305f08c2690ce52f7b8874c4c7c5dde6b1
Leowastakenroblox <115992950+Leowastakenroblox@users.noreply.github.com>
2022-10-19 15:42:33 +0200

    SwordSimulator

---
+++ Swordsimulator.lua
@@ -0,0 +1,55 @@
+_G.autoSword = true
+_G.autoBuy = true
+
+function autoSword()
+    while _G.autoSword == true do
+        game:GetService("Players").LocalPlayer.ninjaEvent:FireServer("swingKatana")
+        wait (0.00000000000000001)
+    end
+end
+
+function autoBuy()
+    while _G.autoBuy == true do
+        game:GetService("Players").LocalPlayer.ninjaEvent:FireServer("buyAllSwords","Ground")
+        game:GetService("Players").LocalPlayer.ninjaEvent:FireServer("buyAllShurikens","Ground")
+        wait (0.25)
+    end
+end
+
+
+local FarmTab = Window:MakeTab({
+Name = "Auto Farm",
+Icon = "rbxassetid://4483345998",
+PremiumOnly = false
+})
+local MiscTab = Window:MakeTab({
+	Name = "Misc",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local CredTab = Window:MakeTab({
+	Name = "Credits",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+FarmTab:AddToggle({
+	Name = "Auto Sword",
+	Default = false,
+	Callback = function(Value)
+		_G.autoSword = Value
+        autoSword()
+	end    
+})
+
+FarmTab:AddToggle({
+	Name = "Auto Buy Sword",
+	Default = false,
+	Callback = function(Value)
+		_G.autoBuy = Value
+        autoBuy()
+	end    
+})
+
+end
+OrionLib:Init()
